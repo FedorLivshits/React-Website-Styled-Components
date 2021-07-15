c0593dc7c791973a40ec233e1f06186ea6be6af0
Fedor Livshits <fediliv@gmail.com>
2021-07-15 22:40:29 +0300

    add NavMenu, NavItem, NavLinks and finished Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { FaMagento, FaTimes, FaBars } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
 import { Link } from 'react-router-dom'
 import { Container } from '../../globalStyles'
 
 const Nav = styled.nav`
 	background: #101522;
-	color: white;
 	height: 80px;
 	display: flex;
 	justify-content: center;
@@ -21,12 +21,11 @@ const NavbarContainer = styled(Container)`
 	display: flex;
 	justify-content: space-between;
 	height: 80px;
-
 	${Container}
 `
 
 const NavLogo = styled(Link)`
-	color: white;
+	color: #fff;
 	justify-self: flex-start;
 	cursor: pointer;
 	text-decoration: none;
@@ -34,12 +33,13 @@ const NavLogo = styled(Link)`
 	display: flex;
 	align-items: center;
 `
+
 const NavIcon = styled(FaMagento)`
-	margin: 0.5rem;
+	margin-right: 0.5rem;
 `
+
 const MobileIcon = styled.div`
 	display: none;
-
 	@media screen and (max-width: 960px) {
 		display: block;
 		position: absolute;
@@ -51,6 +51,56 @@ const MobileIcon = styled.div`
 	}
 `
 
+const NavMenu = styled.ul`
+	display: flex;
+	align-items: center;
+	list-style: none;
+	text-align: center;
+	@media screen and (max-width: 960px) {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 90vh;
+		position: absolute;
+		top: 80px;
+		left: ${({ click }) => (click ? 0 : '-100%')};
+		opacity: 1;
+		transition: all 0.5s ease;
+		background: #101522;
+	}
+`
+
+const NavItem = styled.li`
+	height: 80px;
+	border-bottom: 2px solid transparent;
+	&:hover {
+		border-bottom: 2px solid #4b59f7;
+	}
+	@media screen and (max-width: 960px) {
+		width: 100%;
+		&:hover {
+			border: none;
+		}
+	}
+`
+const NavLinks = styled(Link)`
+	color: #fff;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding: 0.5rem 1rem;
+	height: 100%;
+	@media screen and (max-width: 960px) {
+		text-align: center;
+		padding: 2rem;
+		width: 100%;
+		display: table;
+		&:hover {
+			color: #4b59f7;
+			transition: all 0.3s ease;
+		}
+	}
+`
 const Navbar = () => {
 	const [click, setClick] = useState(false)
 
@@ -58,17 +108,32 @@ const Navbar = () => {
 
 	return (
 		<>
-			<Nav>
-				<NavbarContainer>
-					<NavLogo to='/'>
-						<NavIcon />
-						ULTRA
-					</NavLogo>
-					<MobileIcon onClick={handleClick}>
-						{click ? <FaTimes /> : <FaBars />}
-					</MobileIcon>
-				</NavbarContainer>
-			</Nav>
+			<IconContext.Provider value={{ color: '#fff' }}>
+				<Nav>
+					<NavbarContainer>
+						<NavLogo to='/'>
+							<NavIcon />
+							ULTRA
+						</NavLogo>
+						<MobileIcon onClick={handleClick}>
+							{click ? <FaTimes /> : <FaBars />}
+						</MobileIcon>
+						<NavMenu onClick={handleClick} click={click}>
+							<NavItem>
+								<NavLinks to='/'>Home</NavLinks>
+							</NavItem>
+
+							<NavItem>
+								<NavLinks to='/services'>Services</NavLinks>
+							</NavItem>
+
+							<NavItem>
+								<NavLinks to='/products'>Products</NavLinks>
+							</NavItem>
+						</NavMenu>
+					</NavbarContainer>
+				</Nav>
+			</IconContext.Provider>
 		</>
 	)
 }
