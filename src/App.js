f9e74d08bbde60d13336ba6b0e69e5ef169775aa
Fedor Livshits <fediliv@gmail.com>
2021-07-16 13:17:27 +0300

    add styles in InfoSection component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import {GlobalStyle} from './globalStyles'
 import Navbar from './components/Navbar/Navbar'
+import Home from './pages/HomePage/Home'
 
 function App() {
 	return (
 		<Router>
 			<GlobalStyle />
 			<Navbar />
+			<Switch>
+				<Route path='/' exact component={Home}/>
+			</Switch>
 		</Router>
 	)
 }
---
+++ src/pages/HomePage/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import InfoSection from '../../components/infoSection/infoSection'
+
+const Home = () => {
+	return (
+		<>
+			<InfoSection />
+		</>
+	)
+}
+
+export default Home
