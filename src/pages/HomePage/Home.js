ec69f6b4133fde64b76eddae1f1ee9c13d9e4284
Fedor Livshits <fediliv@gmail.com>
2021-07-16 13:27:46 +0300

    add data for home page in Data.js

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { homeObjOne } from './Data';
 import InfoSection from '../../components/infoSection/infoSection'
 
 const Home = () => {
 	return (
 		<>
-			<InfoSection />
+			<InfoSection {...homeObjOne}/>
 		</>
 	)
 }
